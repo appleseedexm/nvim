baad4b81fddd29f48653f2194e1d38b34078bfc5
appleseedexm <appleseedexm@gmail.com>
2023-08-27 11:10:36 +0200

    removed old failing plugin

--- lua/asx/init.lua
+++ lua/asx/init.lua
@@ -3,21 +3,3 @@ require('asx.plugins')
 require('asx.remap')
 require('asx.set')
 
---local telescope = require('telescope')
---telescope.load_extension('fzf')
---telescope.setup{
---    defaults={		path_display={"smart"}
---	}
---}
-
-
---require('mason').setup()
-
--- Glaive / codefmt
---vim.call('glaive#Install')
---vim.cmd('source /home/dev/.config/nvim/lua/asx/glaive.vim')
-
---  local telescope = require('telescope.builtin')
--- vim.keymap.set('n', 'C-p', telescope.git_files, {})
-
-
