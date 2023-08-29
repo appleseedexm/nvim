89a2c95b5863912a51ccea8b9f338b892a9b5143
appleseedexm <appleseedexm@gmail.com>
2023-08-29 12:43:13 +0200

    unified telescope shortcuts

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,15 +3,48 @@ require('telescope').load_extension('fzf')
 local telescope = require('telescope')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>ts', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>to', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>tr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>trf', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>td', builtin.lsp_type_definitions, {})
+vim.keymap.set('n', '<leader>trs', builtin.resume, {})
+
+local pickers = {
+
+    current_buffer_tags = { fname_width = 100, },
+
+    jumplist = { fname_width = 100, },
+
+    loclist = { fname_width = 100, },
+
+    lsp_definitions = { fname_width = 100, },
+
+    lsp_document_symbols = { fname_width = 100, },
+
+    lsp_dynamic_workspace_symbols = { fname_width = 100, },
+
+    lsp_implementations = { fname_width = 100, },
+
+    lsp_incoming_calls = { fname_width = 100, },
+
+    lsp_outgoing_calls = { fname_width = 100, },
+
+    lsp_references = { fname_width = 100, show_line = false },
+
+    lsp_type_definitions = { fname_width = 100, },
+
+    lsp_workspace_symbols = { fname_width = 100, },
+
+    quickfix = { fname_width = 100, },
+
+    tags = { fname_width = 100, },
+
+}
 
 telescope.setup(
     {
@@ -20,5 +53,7 @@ telescope.setup(
             layout_strategy = "vertical",
             wrap_result = true
         }
+        ,
+        pickers = pickers
     }
 )
