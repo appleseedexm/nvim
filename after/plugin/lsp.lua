1d08671e4cc277e9792c7ee784631e508300e9d5
appleseedexm <appleseedexm@gmail.com>
2024-04-01 18:41:51 +0200

    add gopls to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -88,6 +88,22 @@ local golang_setup = function()
         filetypes = { 'go', 'gomod' },
     }
 
+    -- gopls
+    lspconfig.gopls.setup {
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+            gopls = {
+                completeUnimported = true,
+                usePlaceholders = true,
+                analyses = {
+                    unusedparams = true,
+                },
+            },
+        },
+    }
+
     -- DAP
     dap.adapters.delve = {
         type = 'server',
