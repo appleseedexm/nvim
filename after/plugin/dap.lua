780bb26c57143b8b4734704d90c5a62c6264fc05
appleseedexm <appleseedexm@gmail.com>
2024-06-26 16:07:20 +0200

    add debug attach for java to dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,7 +1,19 @@
-vim.keymap.set("n", "<leader>dr", function() require('dap.repl').toggle({ height = 10 }) end )
-vim.keymap.set("n", "<leader>db", function() require('dap').toggle_breakpoint() end )
-vim.keymap.set("n", "<leader>dsc", function() require('dap').continue() end )
-vim.keymap.set("n", "<leader>dst", function() require('dap').terminate() end )
-vim.keymap.set("n", "<leader>dsi", function() require('dap').step_into() end )
-vim.keymap.set("n", "<leader>dso", function() require('dap').step_out() end )
-vim.keymap.set("n", "<leader>dsv", function() require('dap').step_over() end )
+local dap = require('dap')
+
+vim.keymap.set("n", "<leader>dr", function() require('dap.repl').toggle({ height = 10 }) end)
+vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end)
+vim.keymap.set("n", "<leader>dsc", function() dap.continue() end)
+vim.keymap.set("n", "<leader>dst", function() dap.terminate() end)
+vim.keymap.set("n", "<leader>dsi", function() dap.step_into() end)
+vim.keymap.set("n", "<leader>dso", function() dap.step_out() end)
+vim.keymap.set("n", "<leader>dsv", function() dap.step_over() end)
+
+dap.configurations.java = {
+    {
+        type = 'java',
+        request = 'attach',
+        name = "Debug (Attach) - Remote",
+        hostName = "127.0.0.1",
+        port = 5005,
+    },
+}
