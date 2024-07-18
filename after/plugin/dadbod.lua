86515c68a196031b217973e30705076889c0a0d7
appleseedexm <appleseedexm@gmail.com>
2024-07-18 14:53:05 +0200

    dadbod: run nofoldenable on dbout

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -3,10 +3,17 @@ vim.g.db_ui_execute_on_save = 0
 
 local function setup_sql_completion()
     local cmp = require('cmp')
-    cmp.setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+    cmp.setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
 end
 
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'sql', 'mysql', 'plsql' },
     callback = setup_sql_completion,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'dbout' },
+    callback = function()
+        vim.api.nvim_command([[autocmd FileType dbout setlocal nofoldenable]])
+    end
 })
