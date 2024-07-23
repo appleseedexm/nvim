65ffd71d96b8433aa4e86dad1373622db5735e1d
appleseedexm <appleseedexm@gmail.com>
2024-07-23 08:48:58 +0200

    harpoon use v2, add branch feature, add split shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,6 +1,22 @@
 local harpoon = require("harpoon")
 
-harpoon:setup()
+harpoon:setup({
+    settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = false,
+        key = function()
+            local handle = io.popen("git rev-parse --abbrev-ref HEAD 2> /dev/null")
+            local branch_name = handle:read("*a")
+            handle:close()
+            branch_name = string.gsub(branch_name, "\n", "")
+            if branch_name == nil or branch_name == '' then
+                return vim.loop.cwd()
+            else
+                return branch_name .. vim.loop.cwd()
+            end
+        end,
+    }
+})
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
@@ -19,6 +35,22 @@ local function toggle_telescope(harpoon_files)
         sorter = conf.generic_sorter({}),
     }):find()
 end
+
+harpoon:extend({
+    UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+            harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-x>", function()
+            harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-t>", function()
+            harpoon.ui:select_menu_item({ tabedit = true })
+        end, { buffer = cx.bufnr })
+    end,
+})
 
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
