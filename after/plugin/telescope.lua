8c79de87d85fe29c6b73d744b6555f5076d394d6
appleseedexm <appleseedexm@gmail.com>
2025-03-20 13:48:08 +0100

    telescope optimise symbol search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,13 @@ local previewers = require('telescope.previewers')
 local builtin = require('telescope.builtin')
 local action_layout = require('telescope.actions.layout')
 local actions = require('telescope.actions')
+local fzf_workspace_opts = {
+    fuzzy = false,                  -- false will only do exact matching
+    override_generic_sorter = true, -- override the generic sorter
+    override_file_sorter = true,    -- override the file sorter
+    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+    -- the default case_mode is "smart_case"
+}
 
 local function is_workspace_git_repo()
     return vim.fn.isdirectory(vim.fn.getcwd() .. "/.git") == 1
@@ -47,13 +54,15 @@ vim.keymap.set('n', '<C-p>',
     end,
     {})
 vim.keymap.set('n', '<leader>ts', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > "), additional_args = { '-i.' } })
+    builtin.grep_string({ search = vim.fn.input("Grep string > "), additional_args = { '-i.' } })
+end)
+vim.keymap.set('n', '<leader>tww', function()
+    builtin.lsp_workspace_symbols({ query = vim.fn.input("Grep lsp workspace symbols > ") })
 end)
 vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>to', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>twd', builtin.lsp_dynamic_workspace_symbols, {})
-vim.keymap.set('n', '<leader>tww', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', '<leader>trf', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>td', builtin.lsp_type_definitions, {})
 vim.keymap.set('n', '<leader>trs', builtin.resume, {})
@@ -73,7 +82,7 @@ local pickers = {
 
     lsp_document_symbols = { fname_width = 100, symbol_width = 140, show_line = true },
 
-    lsp_dynamic_workspace_symbols = { fname_width = 100, },
+    lsp_dynamic_workspace_symbols = { fname_width = 100, symbol_width = 140, show_line = true, sorter = telescope.extensions.fzf.native_fzf_sorter(fzf_workspace_opts) },
 
     lsp_implementations = { fname_width = 100, },
 
@@ -85,7 +94,7 @@ local pickers = {
 
     lsp_type_definitions = { fname_width = 100, },
 
-    lsp_workspace_symbols = { fname_width = 100, },
+    lsp_workspace_symbols = { fname_width = 100, symbol_width = 140, show_line = true },
 
     quickfix = { fname_width = 100, },
 
@@ -108,9 +117,8 @@ telescope.setup(
                     ["<C-s>"] = actions.cycle_previewers_next,
                 },
             },
-        }
-        ,
-        pickers = pickers
+        },
+        pickers = pickers,
     }
 )
 
