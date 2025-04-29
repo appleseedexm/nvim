23dd8b53a104e1a59327028981677f8a3863adc7
appleseedexm <appleseedexm@gmail.com>
2025-04-29 11:28:22 +0200

    dadbodui fix keybindings with tmux nav

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -1,6 +1,6 @@
 vim.g.db_ui_use_nerd_fonts = 1
 vim.g.db_ui_execute_on_save = 0
-vim.g.db_ui_disable_mappings = 1
+vim.g.db_ui_disable_mappings = 0
 
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'sql', 'mysql', 'plsql' },
@@ -14,22 +14,23 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'dbui' },
     callback = function()
-        vim.keymap.set('n', 'R', ':normal vip<CR><PLUG>(DBUI_ExecuteQuery)', { buffer = true })
-        vim.keymap.set('n', 'S', '<Plug>(DBUI_SelectLineVsplit)')
-        vim.keymap.set('n', 'R', '<Plug>(DBUI_Redraw)')
-        vim.keymap.set('n', 'd', '<Plug>(DBUI_DeleteLine)')
-        vim.keymap.set('n', 'A', '<Plug>(DBUI_AddConnection)')
-        vim.keymap.set('n', 'H', '<Plug>(DBUI_ToggleDetails)')
-        vim.keymap.set('n', 'r', '<Plug>(DBUI_RenameLine)')
-        vim.keymap.set('n', 'q', '<Plug>(DBUI_Quit)')
-        vim.keymap.set('n', 'K', '<Plug>(DBUI_GotoPrevSibling)')
-        vim.keymap.set('n', 'J', '<Plug>(DBUI_GotoNextSibling)')
-        vim.keymap.set('n', '<C-p>', '<Plug>(DBUI_GotoParentNode)')
-        vim.keymap.set('n', '<C-n>', '<Plug>(DBUI_GotoChildNode)')
+        --vim.keymap.set('n', 'R', ':normal vip<CR><PLUG>(DBUI_ExecuteQuery)', { buffer = true })
+        --vim.keymap.set('n', 'S', '<Plug>(DBUI_SelectLineVsplit)', { buffer = true })
+        --vim.keymap.set('n', '<CR>', '<Plug>(DBUI_SelectLine)', { buffer = true })
+        --vim.keymap.set('n', 'R', '<Plug>(DBUI_Redraw)', { buffer = true })
+        --vim.keymap.set('n', 'd', '<Plug>(DBUI_DeleteLine)', { buffer = true })
+        --vim.keymap.set('n', 'A', '<Plug>(DBUI_AddConnection)', { buffer = true })
+        --vim.keymap.set('n', 'H', '<Plug>(DBUI_ToggleDetails)', { buffer = true })
+        --vim.keymap.set('n', 'r', '<Plug>(DBUI_RenameLine)', { buffer = true })
+        --vim.keymap.set('n', 'q', '<Plug>(DBUI_Quit)', { buffer = true })
+        --vim.keymap.set('n', 'K', '<Plug>(DBUI_GotoPrevSibling)', { buffer = true })
+        --vim.keymap.set('n', 'J', '<Plug>(DBUI_GotoNextSibling)', { buffer = true })
+        --vim.keymap.set('n', '<C-p>', '<Plug>(DBUI_GotoParentNode)', { buffer = true })
+        --vim.keymap.set('n', '<C-n>', '<Plug>(DBUI_GotoChildNode)', { buffer = true })
 
         local nvim_tmux_nav = require('nvim-tmux-navigation')
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { buffer = true })
+        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { buffer = true })
     end
 })
 
