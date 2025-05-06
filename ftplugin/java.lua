3ccf3bc1d05ca7a6b349bfe4e20d4a432ffa3847
Roman <appleseedexm@gmail.com>
2025-05-06 15:26:29 +0200

    Revert "Java config rework"

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,22 +1,22 @@
-local local_jdtls = false
-local local_java_test = false
-
-local home = os.getenv('HOME')
-local jdk17 = vim.fn.expand('~/.sdkman/candidates/java/17.0.14-tem')
-local jdk21 = vim.fn.expand('~/.sdkman/candidates/java/21.0.2-open')
-local jdk = jdk21
-
 local api = vim.api
-local jdtls_install = local_jdtls and home .. "/code/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository" or
-    require('mason-registry')
+local jdtls_install = require('mason-registry')
     .get_package('jdtls')
     :get_install_path()
-local lombok_ver = "lombok-1.18.38.jar"
-local lombok = local_jdtls and home .. "/code/libs/java/" .. lombok_ver or jdtls_install .. "/lombok.jar"
-local sys_arch = vim.loop.os_uname().machine == "aarch64" and "linux_arm" or "linux"
 
--- dap
+
 local dap = require("dap")
+---@type ExecutableAdapter
+dap.adapters.hprof = {
+    type = "executable",
+    --command = os.getenv("GRAALVM_HOME") .. "/bin/java",
+    command = "java",
+    args = {
+        -- "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005",
+        "-Dpolyglot.engine.WarnInterpreterOnly=false",
+        --"-jar",
+        --vim.fn.expand("~/dev/mfussenegger/hprofdap/target/hprofdap-0.1.0-jar-with-dependencies.jar"),
+    }
+}
 dap.configurations.java = {
     {
         type = 'java',
@@ -47,13 +47,14 @@ dap.configurations.java = {
     },
 }
 
--- jdtls
+
+
 local root_markers = { 'gradlew', 'mvnw', '.git' }
 local root_dir = vim.fs.root(0, root_markers) or vim.fs.root(0, { "pom.xml" })
 if not root_dir then
-    print("JDTLS: Could not find rootdir")
     return
 end
+local home = os.getenv('HOME')
 local workspace_folder = home .. "/.local/share/jdtls/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 local jdtls = require('jdtls')
 --jdtls.jol_path = os.getenv('HOME') .. '/apps/jol.jar'
@@ -61,17 +62,6 @@ local config = require('asx.lsp').mk_config({
     root_dir = root_dir,
     settings = {
         java = {
-            import = {
-                gradle = {
-                    enabled = false,
-                    --wrapper = {
-                    --enabled = true
-                    --},
-                    --annotationProcessing = {
-                    --enabled = true
-                    --}
-                },
-            },
             autobuild = { enabled = false },
             maxConcurrentBuilds = 1,
             signatureHelp = { enabled = true },
@@ -112,8 +102,7 @@ local config = require('asx.lsp').mk_config({
                     template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
                 },
                 hashCodeEquals = {
-                    useJava7Objects = false,
-                    useInstanceOf = true,
+                    useJava7Objects = true,
                 },
                 useBlocks = true,
                 addFinalForNewDeclaration = "fields",
@@ -122,23 +111,20 @@ local config = require('asx.lsp').mk_config({
                 runtimes = {
                     {
                         name = 'JavaSE-17',
-                        path = jdk17,
-                        default = true,
+                        path = vim.fn.expand('~/.sdkman/candidates/java/17.0.14-tem'),
                     },
                     {
                         name = 'JavaSE-21',
-                        path = jdk21,
+                        path = vim.fn.expand('~/.sdkman/candidates/java/21.0.2-open'),
                     },
                 }
             },
-            symbols = {
-                includeSourceMethodDeclarations = true
-            },
+            symbols = { includeSourceMethodDeclarations = true },
         }
     },
     cmd = {
-        jdk .. "/bin/java",
-        "-javaagent:" .. lombok,
+        "java",
+        "-javaagent:" .. jdtls_install .. '/lombok.jar',
         --'-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044',
         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
         '-Dosgi.bundles.defaultStartLevel=4',
@@ -146,38 +132,15 @@ local config = require('asx.lsp').mk_config({
         '-Dlog.protocol=true',
         '-Dlog.level=ALL',
         '-Xmx4g',
-        '-XX:+AlwaysPreTouch',
         '--add-modules=ALL-SYSTEM',
         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        '-jar', vim.fn.glob(jdtls_install .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+        '-configuration', jdtls_install .. '/config_linux',
+        '-data', workspace_folder,
     }
 })
 
-if sys_arch == 'linux' then
-    vim.list_extend(config.cmd, {
-        '-XX:+UseTransparentHugePages',
-    })
-end
-
-local launcher = vim.fn.glob(jdtls_install .. '/plugins/org.eclipse.equinox.launcher_*.jar')
-if vim.uv.fs_stat(launcher) then
-    vim.list_extend(config.cmd, {
-        "-jar", launcher,
-        "-configuration", jdtls_install .. "/config_" .. sys_arch,
-        "-data", workspace_folder
-    })
-else
-    print("JDTLS no equinox launcher found")
-    return
-    --vim.list_extend(config.cmd, {
-    --"-Dosgi.checkConfiguration=true",
-    --"-Dosgi.sharedConfiguration.area=/usr/share/java/jdtls/config_linux/",
-    --"-Dosgi.sharedConfiguration.area.readOnly=true",
-    --"-Dosgi.configuration.cascaded=true",
-    --"-jar", vim.fn.glob("/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-    --"-data", workspace_folder,
-    --})
-end
 
 local function test_with_profile(test_fn)
     return function()
@@ -201,16 +164,15 @@ local function test_with_profile(test_fn)
             local async_profiler_so = home .. "/apps/async-profiler/lib/libasyncProfiler.so"
             local event = 'event=' .. choice
             local vmArgs = "-ea -agentpath:" .. async_profiler_so .. "=start,"
-            vmArgs = vmArgs .. event .. ",collapsed,file=/tmp/jdtls_traces.txt"
+            vmArgs = vmArgs .. event .. ",file=/tmp/profile.jfr"
             test_fn({
                 config_overrides = {
                     vmArgs = vmArgs,
                     noDebug = true,
                 },
                 after_test = function()
-                    vim.cmd.tabnew()
-                    vim.fn.jobstart({ "flamelens", "/tmp/jdtls_traces.txt" }, { term = true })
-                    vim.cmd.startinsert()
+                    vim.fn.system("jfr2flame /tmp/profile.jfr /tmp/profile.html")
+                    vim.fn.system("firefox /tmp/profile.html")
                 end
             })
         end)
@@ -218,16 +180,12 @@ local function test_with_profile(test_fn)
 end
 
 config.on_attach = function(client, bufnr)
-    local function compile()
-        if vim.bo.modified then
-            vim.cmd("w")
-        end
-        client.request_sync("java/buildWorkspace", false, 5000, bufnr)
-    end
-
     local function with_compile(fn)
         return function()
-            --compile()
+            if vim.bo.modified then
+                vim.cmd("w")
+            end
+            client.request_sync("java/buildWorkspace", false, 5000, bufnr)
             fn()
         end
     end
@@ -235,21 +193,7 @@ config.on_attach = function(client, bufnr)
     api.nvim_buf_create_user_command(bufnr, "A", function()
         require("jdtls.tests").goto_subjects()
     end, {})
-
-
-    local opts = { silent = true, buffer = bufnr }
-    local set = vim.keymap.set
-    set("n", "<F5>", function()
-        if dap.session() == nil then
-            compile()
-            dap.continue()
-        else
-            dap.continue()
-        end
-    end, opts)
-
-    api.nvim_buf_create_user_command(
-        bufnr,
+    api.nvim_create_user_command(
         "RelativeCodeFormat",
         function()
             vim.cmd("FormatCode")
@@ -257,31 +201,43 @@ config.on_attach = function(client, bufnr)
         {}
     )
 
+    local triggers = vim.tbl_get(client.server_capabilities, "completionProvider", "triggerCharacters")
+    if triggers then
+        for _, char in ipairs({ "a", "e", "i", "o", "u" }) do
+            if not vim.tbl_contains(triggers, char) then
+                table.insert(triggers, char)
+            end
+        end
+    end
+    vim.lsp.completion.enable(false, client.id, bufnr, { autotrigger = true })
+
+    local opts = { silent = true, buffer = bufnr }
+    local set = vim.keymap.set
+
 
     -- debug / test
-    local conf_overrides = {
-        stepFilters = {
-            skipClasses = { "$JDK", "junit.*" },
-            skipSynthetics = true
-        },
-        vmArgs = table.concat({
-            "-ea",
-            "-XX:+TieredCompilation",
-            "-XX:TieredStopAtLevel=1",
-            "--enable-native-access=ALL-UNNAMED",
-        }, " "),
-    }
-
     set("n", "<leader>dsc", with_compile(function()
         dap.continue()
     end), opts)
     set('n', "<leader>df", with_compile(function()
-        jdtls.test_class({ config_overrides = conf_overrides })
+        jdtls.test_class({
+            config_overrides = {
+                vmArgs = "-ea -XX:+TieredCompilation -XX:TieredStopAtLevel=1",
+            }
+        })
     end), opts)
     set('n', "<leader>dl", with_compile(require("dap").run_last), opts)
     set('n', "<leader>dF", with_compile(test_with_profile(jdtls.test_class)), opts)
     set('n', "<leader>dn", with_compile(function()
-        jdtls.test_nearest_method({ config_overrides = conf_overrides })
+        jdtls.test_nearest_method({
+            config_overrides = {
+                stepFilters = {
+                    skipClasses = { "$JDK", "junit.*" },
+                    skipSynthetics = true
+                },
+                vmArgs = "-ea -XX:+TieredCompilation -XX:TieredStopAtLevel=1",
+            }
+        })
     end), opts)
     set('n', "<leader>dN", with_compile(test_with_profile(jdtls.test_nearest_method)), opts)
     set('n', '<leader>dj', require('jdtls.tests').goto_subjects, opts)
@@ -305,68 +261,29 @@ config.on_attach = function(client, bufnr)
     set('n', "crc", jdtls.extract_constant, opts)
 end
 
----@return table
-local function bundle_jar_patterns()
-    local java_debug_path = require('mason-registry')
-        .get_package('java-debug-adapter')
-        :get_install_path() .. '/extension/server'
-    local java_decomp_path = require('mason-registry')
-        .get_package('vscode-java-decompiler')
-        :get_install_path() .. '/server'
-    local jar_patterns = {
-        java_debug_path .. '/com.microsoft.java.debug.plugin-*.jar',
-        java_decomp_path .. '/*.jar',
-    }
+
+local java_test_path = require('mason-registry')
+    .get_package('java-test')
+    :get_install_path() .. '/extension/server'
 
 
-    -- Add java-test
-    if local_java_test then
-        vim.list_extend(jar_patterns,
-            {
-                home .. '/code/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.plugin/target/*.jar',
-                home .. '/code/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.runner/target/*.jar',
-                home .. '/code/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.runner/lib/*.jar',
-                --home .. '/code/microsoft/vscode-java-test/server/*.jar',
-            })
-        local plugin_path =
-            home ..
-            '/code/microsoft/vscode-java-test/java-extension/com.microsoft.java.test.plugin.site/target/repository/plugins/'
-        local bundle_list = vim.tbl_map(
-            function(x) return require('jdtls.path').join(plugin_path, x) end,
-            {
-                'junit-jupiter-*.jar',
-                'junit-platform-*.jar',
-                'junit-vintage-engine_*.jar',
-                'org.opentest4j*.jar',
-                'org.apiguardian.api_*.jar',
-                'org.eclipse.jdt.junit4.runtime_*.jar',
-                'org.eclipse.jdt.junit5.runtime_*.jar',
-                'org.opentest4j_*.jar',
-                'org.jacoco.*.jar',
-                'org.objectweb.asm*.jar'
-            }
-        )
-        vim.list_extend(jar_patterns, bundle_list)
-    else
-        local java_test_path = require('mason-registry')
-            .get_package('java-test')
-            :get_install_path() .. '/extension/server'
-        vim.list_extend(jar_patterns, {
-            java_test_path .. '/*.jar',
-        })
-    end
-    return jar_patterns
-end
+local java_debug_path = require('mason-registry')
+    .get_package('java-debug-adapter')
+    :get_install_path() .. '/extension/server'
 
-local jar_patterns = bundle_jar_patterns()
+
+local jar_patterns = {
+    java_debug_path .. '/com.microsoft.java.debug.plugin-*.jar',
+    --'/dev/dgileadi/vscode-java-decompiler/server/*.jar',
+    java_test_path .. '/*.jar',
+    --'/dev/testforstephen/vscode-pde/server/*.jar'
+}
+
 local bundles = {}
 for _, jar_pattern in ipairs(jar_patterns) do
     for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), '\n')) do
-        if true
-            and not vim.endswith(bundle, 'com.microsoft.java.test.runner-jar-with-dependencies.jar')
-            and not vim.endswith(bundle, 'com.microsoft.java.test.runner.jar')
-            and bundle ~= ""
-        then
+        if not vim.endswith(bundle, 'com.microsoft.java.test.runner-jar-with-dependencies.jar')
+            and not vim.endswith(bundle, 'com.microsoft.java.test.runner.jar') then
             table.insert(bundles, bundle)
         end
     end
