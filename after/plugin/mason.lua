c75ddcd0de147007cc22c748c19addc788f0dfd4
appleseedexm <appleseedexm@gmail.com>
2025-05-28 19:41:45 +0200

    mason

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -5,25 +5,45 @@ for _, pkg_name in ipairs {
     'java-debug-adapter',
     'java-test',
     'jdtls',
+    'vscode-java-decompiler',
     -- Kotlin
     'ktlint',
+    'kotlin-language-server',
     -- XML
     'lemminx',
     -- Lua
     'lua-language-server',
-    -- Kotlin
-    'kotlin-language-server',
     -- Rust
     'rust-analyzer',
+    -- Zig
+    'zls',
+    -- LLVM
     'codelldb',
+    -- Lua
+    'lua-language-server',
+    'luaformatter',
     -- Golang
     'delve',
     'gopls',
+    'golangci-lint',
+    'golangci-lint-langserver',
+    -- Python
+    'python-lsp-server',
+    -- CSS
+    'css-lsp',
+    -- GitLab
+    'gitlab-ci-ls',
+    -- JSON
+    'json-lsp',
+    -- HTML
+    'html-lsp',
     -- JS/TS
     'eslint-lsp',
     'angular-language-server',
     'prettier',
-    'typescript-language-server', } do
+    'typescript-language-server',
+    'js-debug-adapater',
+} do
     local ok, pkg = pcall(registry.get_package, pkg_name)
     if ok then
         if not pkg:is_installed() then
