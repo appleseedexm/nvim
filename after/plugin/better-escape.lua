0f85f84f39d20b5a00b824d830ae1e63a33ef4fe
appleseedexm <appleseedexm@gmail.com>
2025-06-18 13:05:56 +0200

    better escape disable default mappings

--- after/plugin/better-escape.lua
+++ after/plugin/better-escape.lua
@@ -1,48 +1,48 @@
 -- lua, default settings
 require("better_escape").setup {
     timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
-    default_mappings = true,    -- setting this to false removes all the default mappings
+    default_mappings = false,    -- setting this to false removes all the default mappings
     mappings = {
         -- i for insert
         i = {
-            j = {
-                -- These can all also be functions
-                --k = "<Esc>",
-                --j = "<Esc>",
-            },
+            --j = {
+                ---- These can all also be functions
+                ----k = "<Esc>",
+                ----j = "<Esc>",
+            --},
             [" "] = {
                 [" "] = "<Esc>",
             },
         },
         c = {
-            j = {
-                --k = "<C-c>",
-                --j = "<C-c>",
-            },
+            --j = {
+                ----k = "<C-c>",
+                ----j = "<C-c>",
+            --},
             [" "] = {
                 [" "] = "<Esc>",
             },
         },
         t = {
-            j = {
-                --k = "<C-\\><C-n>",
-            },
+            --j = {
+                ----k = "<C-\\><C-n>",
+            --},
             [" "] = {
                 [" "] = "<Esc>",
             },
         },
         v = {
-            j = {
-                --k = "<Esc>",
-            },
+            --j = {
+                ----k = "<Esc>",
+            --},
             [" "] = {
                 [" "] = "<Esc>",
             }
         },
         s = {
-            j = {
-                --k = "<Esc>",
-            },
+            --j = {
+                ----k = "<Esc>",
+            --},
             [" "] = {
                 [" "] = "<Esc>",
             }
