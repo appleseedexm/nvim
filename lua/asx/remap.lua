9c3918ba5ebc04f514cad53415e32f0e90f7997c
appleseedexm <appleseedexm@gmail.com>
2025-06-18 11:28:59 +0200

    cleanup remap

--- lua/asx/remap.lua
+++ lua/asx/remap.lua
@@ -18,8 +18,6 @@ vim.keymap.set("n", "<leader>ll", ":redir @+ | echon '`' . expand('%:h') . '/' .
 -- zoom in new tab
 vim.keymap.set("n", "<C-w>z", ":tab split<CR>")
 
--- primagen conf
---
 -- move code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -45,9 +43,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- delete into void register
 vim.keymap.set({ "n", "v" }, "<leader>-", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tms<CR>")
 
@@ -57,12 +52,3 @@ vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<C-x>", ":lclose<CR>:cclose<CR>")
 
---vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
---
---vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
---vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
