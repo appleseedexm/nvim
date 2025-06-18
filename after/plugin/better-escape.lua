e94e5fa23db2c9cbb1a150a79bd682033c4f3fed
appleseedexm <appleseedexm@gmail.com>
2025-06-18 11:28:45 +0200

    better escape double space

--- after/plugin/better-escape.lua
+++ after/plugin/better-escape.lua
@@ -1,36 +1,51 @@
 -- lua, default settings
 require("better_escape").setup {
     timeout = vim.o.timeoutlen, -- after `timeout` passes, you can press the escape key and the plugin will ignore it
-    default_mappings = true, -- setting this to false removes all the default mappings
+    default_mappings = true,    -- setting this to false removes all the default mappings
     mappings = {
         -- i for insert
         i = {
             j = {
                 -- These can all also be functions
-                k = "<Esc>",
+                --k = "<Esc>",
                 --j = "<Esc>",
+            },
+            [" "] = {
+                [" "] = "<Esc>",
             },
         },
         c = {
             j = {
-                k = "<C-c>",
+                --k = "<C-c>",
                 --j = "<C-c>",
+            },
+            [" "] = {
+                [" "] = "<Esc>",
             },
         },
         t = {
             j = {
-                k = "<C-\\><C-n>",
+                --k = "<C-\\><C-n>",
+            },
+            [" "] = {
+                [" "] = "<Esc>",
             },
         },
         v = {
             j = {
-                k = "<Esc>",
+                --k = "<Esc>",
             },
+            [" "] = {
+                [" "] = "<Esc>",
+            }
         },
         s = {
             j = {
-                k = "<Esc>",
+                --k = "<Esc>",
             },
+            [" "] = {
+                [" "] = "<Esc>",
+            }
         },
     },
 }
