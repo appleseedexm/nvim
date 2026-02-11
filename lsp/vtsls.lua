a0bde27bca0b66114bfa302777d004a581bf1856
appleseedexm <appleseedexm@gmail.com>
2026-02-11 17:26:53 +0100

    vtsls split up async methods until single organizeImports works again

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,13 +1,27 @@
 return require('asx.lsp').mk_config({
     on_attach = function(client, bufnr)
-        vim.keymap.set('n', '<leader>co', '<cmd>OrganizeImports<cr>', { buffer = bufnr })
+        -- local command = "_typescript.organizeImports"
+        -- local filename = vim.api.nvim_buf_get_name(bufnr)
+        -- client:request(command, { filename }, nil, bufnr)
+
+        vim.keymap.set('n', '<leader>coo', '<cmd>OrganizeImports<cr>', { buffer = bufnr })
         vim.api.nvim_create_user_command(
             "OrganizeImports",
             function()
-                -- local command = "_typescript.organizeImports"
-                -- local filename = vim.api.nvim_buf_get_name(bufnr)
-                -- client:request(command, { filename }, nil, bufnr)
+                vim.lsp.buf.code_action({
+                    apply = true,
+                    context = {
+                        only = { "source.organizeImports" },
+                    }
+                })
+            end,
+            {}
+        )
 
+        vim.keymap.set('n', '<leader>coi', '<cmd>OrganizeMissingImports<cr>', { buffer = bufnr })
+        vim.api.nvim_create_user_command(
+            "OrganizeMissingImports",
+            function()
                 vim.lsp.buf.code_action({
                     apply = true,
                     context = {
